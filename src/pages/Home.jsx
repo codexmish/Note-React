f2f19426c23aaf52b995fced412aecb4615e4873
codexmish <majharulislam.shanto.70@gmail.com>
2025-11-16 11:33:59 +0600

    home not homing----

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,49 @@
-import React, { useState } from 'react'
-import { getDatabase, ref, set } from "firebase/database";
+import React, { useEffect, useState } from "react";
+import { getDatabase, onValue, push, ref, set } from "firebase/database";
 
 const Home = () => {
-
-  const [inputData, setInputData] = useState('')
+  const [inputData, setInputData] = useState("");
 
   const db = getDatabase();
 
-  const handalSubmit = (e)=>{
-    e.preventDefault()
+  const handalSubmit = (e) => {
+    e.preventDefault();
 
-    set(ref(db, 'users/'), {
-    data: inputData
-  });
+    // set data to firebase
 
+    set(push(ref(db, "users/")), {
+      data: inputData,
+    });
+  };
 
-  }
+  // Get data from firebase
 
+  useEffect(() => {
+    onValue(ref(db, "users/"), (snapshot) => {
+
+      let myArr = []
+      const data = snapshot.val();
+      data.forEach((item)=>{
+        myArr.push(item)
+      })
+      console.log(myArr)
+    });
+  }, []);
 
   return (
     <>
-    <div className="container mt-20">
-
-    <form onSubmit={handalSubmit} action="">
-      <input onChange={(e)=>setInputData(e.target.value)} type="text" className='border '/>
-      <button>Go</button>
-    </form>
-    </div>
-    
+      <div className="container mt-20">
+        <form onSubmit={handalSubmit} action="">
+          <input
+            onChange={(e) => setInputData(e.target.value)}
+            type="text"
+            className="border "
+          />
+          <button>Go</button>
+        </form>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
